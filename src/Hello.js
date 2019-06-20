9ad67c5240ebe0680bfd1ca254a121bba41682c2
peteyjoe <github@peteyjoe.com>
2019-06-20 07:35:52 -0700

    Initial Commit

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Hello.css';
+import 'tachyons';
+
+class Hello extends Component {
+    render(){
+        return (
+        <div>
+            <h1 className="f1 tc">Hello World</h1>
+            <p>{this.props.greeting}</p>
+        </div>
+        );
+    }
+}
+
+export default 
