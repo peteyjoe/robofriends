761243993692a2a98da3e54f5d9afae28c72652d
peteyjoe <github@peteyjoe.com>
2019-06-20 07:47:01 -0700

    changes

--- src/Hello.js
+++ src/Hello.js
@@ -2,28 +2,16 @@ import React, { Component } from 'react';
 import './Hello.css';
 import 'tachyons';
 
-if (1>2) {
-    class Hello extends Component {
-        render(){
-            return (
-            <div>
-                <h1 className="f1 tc">Hello World</h1>
-                <p>{this.props.greeting}</p>
-            </div>
-            );
-        }
-    }
-} else {
-    class Hello extends Component {
-        render(){
-            return (
-            <div>
-                <h1 className="f1 tc">Welcome!</h1>
-                <p>{this.props.greeting}</p>
-            </div>
-            );
-        }
-    }
-}
+
+class Hello extends Component {
+ render(){
+    return (
+        <div>
+            <h1 className="f1 tc">Hello World</h1>
+            <p>{this.props.greeting}</p>
+        </div>
+        );
+     }
+ }
 
 export default Hello;
