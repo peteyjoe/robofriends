2cc65fcacc1896b0c3a2aecdda4ce28e457e006e
peteyjoe <github@peteyjoe.com>
2019-06-20 07:58:03 -0700

    Starting from scratch

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src='' alt='Photo' />
+        </div>
+        <h2>Jane Doe</h2>
+        <p>janedoe@email.com</p>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello.js';
+import Card from './Card.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello greeting={'Hello React Ninja'}/>, document.getElementById('root'));
-
+ReactDOM.render(<Card />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
