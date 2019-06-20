9be528065013c24f2d6aa6137f2efbd2e7ce5b27
peteyjoe <github@peteyjoe.com>
2019-06-20 10:39:43 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js';
+import Card from './Card.js.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
