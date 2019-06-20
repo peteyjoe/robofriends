77157fa5fd4d555fe557a7cfc28c4819595d05a6
peteyjoe <github@peteyjoe.com>
2019-06-20 07:40:39 -0700

    Changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Hello from './Hello.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello React Ninja'}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
